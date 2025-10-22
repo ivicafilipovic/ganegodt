bb6395a6c8f23de72350033e9d1778e5e71db96d
Ivi <ivica.filipovic@bluewin.chgit config --global user.git config --global user.email ivica.filipovic@bluewin.ch>
2025-10-22 21:18:40 +0200

    accepted incoming changes

--- js/main.js
+++ js/main.js
@@ -1,52 +1,3 @@
-<<<<<<< HEAD
-///////Gridrezepte//////
-//Damit man nur die Zahl bei data-skill ändern kann und dann wird die Anzahl Bilder veränder
-var skillContainer = $(".rezeptSkills"); //
-skillContainer.each(function () {
-  // "each"wird für jedes Element mit dem Element in der definieren Variable ausgeführt
-  var skillLevel = $(this).data("skill"); //"this" wird nur auf dieses Element angwendet
-  var iconsContainer = $(this).find(".skillIcons");
-
-  for (var i = 0; i < skillLevel; i++) {
-    var img = $("<img>", {
-      src: "./assets/icons/chefSVG.svg",
-      alt: "Female Chef",
-      class: "skillImg",
-    });
-    iconsContainer.append(img);
-  }
-});
-
-///////Footer///////
-//SocialMediaFooter//
-var icon = $(".fa-brands, .fa-square-envelope");
-icon.on("mouseenter", function () {
-  $(this).addClass("fa-bounce");
-});
-icon.on("mouseleave", function () {
-  $(this).removeClass("fa-bounce");
-});
-
-//Logo Beat Fade Animation//
-//document.ready heisst erst Function ausführen wenn ganzes HTML geladen wird
-$(document).ready(function () {
-  //
-  $(".footerLogo").on("mouseenter", function () {
-    $(this).addClass("bounce");
-  });
-  $(".footerLogo").on("mouseleave", function () {
-    $(this).removeCLass("bounce");
-  });
-});
-
-//Damit die Animation bei jedem Hover neu startet//
-$(".footerLogo").on("mouseenter", function () {
-  var logbounce = $(this);
-  logbounce.removeClass("bounce"); //Alte Animatoon entfernen
-  void logbounce[0].offsetWidth; // Reflow erzwingen
-  logbounce.addClass("bounce"); //Animation starten
-});
-=======
 ///////Gridrezepte//////
 //Damit man nur die Zahl bei data-skill ändern kann und dann wird die Anzahl Bilder veränder
 var skillContainer = $(".rezeptSkills"); //
@@ -94,4 +45,3 @@ $(".footerLogo").on("mouseenter", function () {
   void logbounce[0].offsetWidth; // Reflow erzwingen
   logbounce.addClass("bounce"); //Animation starten
 });
->>>>>>> a0b5974ddaec58aa5713eee0e89cc6032497c1cd
